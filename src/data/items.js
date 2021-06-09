cee8c52f5cee5f7e9beb9414dd8b6200f43401d7
Uday <udaysaraswat2@gmail.com>
2021-06-09 20:16:43 +0530

    initial commit

---
+++ src/data/items.js
@@ -0,0 +1,17 @@
+const items = [
+    {
+        id:1,
+        name:"varun",
+        price:1200,
+        category:'footwear',
+        description: 'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group',
+    },
+    {
+        id:2,
+        name:"uday",
+        price:400,
+        category:'shirt',
+        description: 'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group',
+    },
+]
+export default items
